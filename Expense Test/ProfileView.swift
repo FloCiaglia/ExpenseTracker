d1f2c295e75c4b5898b8bb31af6552acbbf18b39
FloCiaglia <floriana.ciaglia@gmail.com>
2020-10-26 15:44:51 -0600

    Added a ImagePicker view

--- Expense Test/ProfileView.swift
+++ Expense Test/ProfileView.swift
@@ -9,8 +9,48 @@
 import SwiftUI
 
 struct ProfileView: View {
+    
+    @State private var showSheet:Bool = false
+    @State private var showImagePicker: Bool = false
+    @State private var sourceType: UIImagePickerController.SourceType = .camera
+    
+    @State private var image: UIImage?
+    
+    
     var body: some View {
-        Text("Hello, Profile!")
+        NavigationView{
+            
+            VStack{
+                
+                Image(uiImage: ((image ?? UIImage(named: "camera-icon"))!))
+                    .resizable()
+                    .frame(width: 120, height: 120)
+                
+                Button("choose picture"){
+                    self.showSheet = true
+                    
+                }.padding()
+                .actionSheet(isPresented: $showSheet) {
+                    ActionSheet(title: Text("Select Photo"), message: Text("choose"), buttons: [.default(Text("Photo Library")){
+                        
+                        self.showImagePicker = true
+                        self.sourceType = .photoLibrary
+                    },
+                    .default(Text("Camera")){
+                        
+                        self.showImagePicker = true
+                        self.sourceType = .camera
+                    },
+                    .cancel()
+                    ])
+                    
+                
+            }
+            
+            .navigationBarTitle("MyProfile")
+            }.sheet(isPresented: $showImagePicker) {
+                ImagePicker(image: self.$image, isShown: self.$showImagePicker, sourceType: self.sourceType)
+            }
     }
 }
 
@@ -18,4 +58,15 @@ struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
         ProfileView()
     }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Image("fall-leaves")
+            .resizable()
+            .scaledToFit()
+    }
+}
+
+
 }
